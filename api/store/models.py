cb1046e9dd38e05939e57c2f5b0ab758bb2414fc
Alangard <73427768+Alangard@users.noreply.github.com>
2024-02-08 20:31:59 +0300

    A store page has been created

--- api/store/models.py
+++ api/store/models.py
@@ -7,7 +7,7 @@ class Product(models.Model):
     product_name = models.CharField(max_length = 200, unique = True)
     slug = models.SlugField(max_length = 200, unique = True)
     description = models.TextField(max_length = 500, blank = True)
-    price = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     product_image = models.ImageField(upload_to = 'photos/products/')
     stock = models.PositiveIntegerField()
     is_available = models.BooleanField(default = True)

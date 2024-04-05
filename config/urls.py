4524f86c85d9f9fef512a65837d2f09063d42c1f
Alangard <73427768+Alangard@users.noreply.github.com>
2024-04-05 21:54:58 +0300

    Ngnix configuration
    Added ngnix configuration file (ngnix.conf)
    Updated docker-compose

--- config/urls.py
+++ config/urls.py
@@ -30,4 +30,7 @@ urlpatterns = [
     path('orders/', include('api.orders.urls')),
     
     path("__debug__/", include("debug_toolbar.urls")),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)

f242065b347db5332f6ed94a9a3d99c5b6f05415
Alangard <73427768+Alangard@users.noreply.github.com>
2024-04-12 20:03:24 +0300

    update

--- config/settings.py
+++ config/settings.py
@@ -15,9 +15,8 @@ import os
 from dotenv import load_dotenv
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(os.path.join(BASE_DIR, './.env/.env.prod')) 
+
+load_dotenv('./.env/.env.prod') 
 
 
 # Quick-start development settings - unsuitable for production
